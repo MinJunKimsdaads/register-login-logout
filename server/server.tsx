112e626294aa7ba0e2af975dd9375c81d4ded609
MinJunKimsdaads <aaeexx@naver.com>
2023-08-29 15:45:35 +0900

    20230829

--- server/server.tsx
+++ server/server.tsx
@@ -6,6 +6,7 @@ const cors = require("cors");
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended:true}));
+//extended 옵션의 경우, true일 경우, 객체 형태로 전달된 데이터내에서 또다른 중첩된 객체를 허용한다는 말이며, false인 경우에는 허용하지 않는 의미
 app.use(bodyParser.json());
 app.use(cors());
 
