e7f7b3ba750fae67ee079cdf17b658ca19e7b08d
KMJ <aaeexx@naver.com>
2023-09-21 22:55:05 +0900

    20230921

--- server/db.tsx
+++ server/db.tsx
@@ -1,18 +1,18 @@
 //조회 -> 아이디 중복 체크 후 저장
 //패스워드 암호화 필요
-const mongoose = require('mongoose');
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'alswns123!A',
+    database : 'justright'
+});
 
-const connect = () => {
-    // console.log(process.env.DATABASE);
-    mongoose
-    .connect(
-        process.env.DATABASE
-    ).then(()=>{
-        console.log('mongodb connected')
-    })
-    .catch((err)=>{
-        console.log("error:" + err);
-    })
-}
+connection.connect();
 
-module.exports = connect;
+connection.query('SELECT * from user_info', (error, rows, fields) => {
+    if (error) throw error;
+    console.log('User info is: ', rows);
+});
+
+connection.end();
