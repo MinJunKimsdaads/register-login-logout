1186df32b1da21f06d7e1506ddd73de621098cf3
KMJ <aaeexx@naver.com>
2023-10-17 22:24:21 +0900

    ddd

--- server/db/dbConfig.tsx
+++ server/db/dbConfig.tsx
@@ -1,6 +1,6 @@
 const env = require("dotenv").config({ path: ".env" });
 module.exports = {
-    host     : '127.0.0.1',
+    host     : '192.168.219.1',
     user     : 'user1',
     port: '3306',
     password : 'alswns123!',
